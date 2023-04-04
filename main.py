2c24ccbf8acb2ccd167efbd7f90458302e17838b
Adria Barreto <adriabarreto12@gmail.com>
2023-04-04 16:28:24 -0300

    Corrige erros

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ numero = int(numero)
 resto3 = int( numero % 3)
 resto5 = int( numero % 5)
 
-if resto3 == 0:
+if resto3 == 0 and resto5 == 0:
+    print("FizzBuzz")
+elif resto3 == 0:
     print("Fizz")
 elif resto5 == 0:
     print("Buzz")
-elif resto3 == 0 and resto5 == 0:
-    print("FizzBuzz")
+
 else:
     print(numero)
